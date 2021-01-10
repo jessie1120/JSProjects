13045a0ee459226527372e856112efb3e12494c1
JesseFang <marvin99988@gmail.com>
2021-01-11 00:29:28 +0800

    finish calculate function & event part

---
+++ 3_exchange_rate_calculator/script.js
@@ -0,0 +1,30 @@
+function calculate() {
+    const currency_one = $('#currency-one').val();
+    const currency_two = $('#currency-two').val();
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    // 
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        const rate = data.rates[currency_two];
+        $('#rate').text(`1 ${currency_one} = ${rate} ${currency_two}`);
+
+        $('#amount-two').val(($('#amount-one').val() * rate).toFixed(4)); 
+    });
+}
+// Eventlistener
+$('#currency-one').change(function() { 
+    calculate(); 
+});
+$('#currency-two').change(function() {
+    calculate();
+});
+$('#amout-one').change(function() {
+    calculate();
+});
+$('#amout-two').change(function() {
+    calculate();
+});
+calculate();
