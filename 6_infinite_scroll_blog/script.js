05d17cfbb7d38a4e3c432c2bb91ca5e900152916
JesseFang <marvin99988@gmail.com>
2021-01-15 04:46:12 +0800

    use Data to create DOM

--- 6_infinite_scroll_blog/script.js
+++ 6_infinite_scroll_blog/script.js
@@ -1,11 +1,34 @@
 let limit = 5
 let page = 1
 
-$.ajax({
-    url: `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`,
-    method: 'get',
-    dataType: 'json',
-    success: function (data) {
-      console.log(data);
-    }  
-})
+function showPosts() {
+    $.ajax({
+        url: `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`,
+        method: 'get',
+        dataType: 'json',
+        success: function (data) {
+            $.each(data,function(index,post){
+                const postDiv = $('<div/>').addClass('post');
+                postDiv.html(`
+                <div class="number">${post.id}</div>
+                <div class="post-info">
+                  <h2 class="post-title">${post.title}</h2>
+                  <p class="post-body">${post.body}</p>
+                </div>
+              `)
+                $('#posts-container').append(postDiv);
+            })
+            // data.forEach(post => {
+            //     const postDiv = $('<div/>').addClass('post');
+            //     postDiv.html(`
+            //     <div class="number">${post.id}</div>
+            //     <div class="post-info">
+            //       <h2 class="post-title">${post.title}</h2>
+            //       <p class="post-body">${post.body}</p>
+            //     </div>
+            //   `)
+            //     $('#posts-container').append(postDiv);
+            // })
+        }
+    })
+}
