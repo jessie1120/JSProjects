b97a4c7b0f390590b32d496a0417d1b9c62e5ce1
JesseFang <marvin99988@gmail.com>
2021-01-19 22:17:57 +0800

    type word check and add score

--- 7_typing_game/script.js
+++ 7_typing_game/script.js
@@ -1,12 +1,26 @@
+let score = 0
+
 function getRandomWord() {
     $.ajax({
-        url: 'https://random-word-api.herokuapp.com/word?number=100',
+        url: 'https://random-word-api.herokuapp.com/word?number=1',
         method: 'get',
         dataType: 'json',
         success: function (data) {
-          let randomWord = data[Math.floor(Math.random() * data.length)]
-          $('h1').text(randomWord);
+          $('h1').text(data);
         }  
     })
 }
 getRandomWord();
+
+// Event Listener
+
+$('#text').on('input', function() {
+    const typeWord = $(this).val()
+    const randomWord = $('h1').text()
+    if (typeWord === randomWord) {
+        getRandomWord();
+        $(this).val('');
+        score++
+        $('#score').text(score);  
+    }
+})
