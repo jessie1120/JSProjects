9c36e32caf5ed9a7dacf923cba970a2e45cd9a4e
JesseFang <marvin99988@gmail.com>
2021-01-20 22:15:32 +0800

    get voice and put into DOM

--- 9_speak_number_guess/script.js
+++ 9_speak_number_guess/script.js
@@ -3,4 +3,27 @@ console.log(randomNumber)
 
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
-  }
+}
+
+function getSpeakWord(e) {
+    const msg = e.results[0][0].transcript;
+    showMessage(msg);
+}
+
+function showMessage(msg) {
+    $('#msg').html(`
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+    `)
+}
+
+window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+recognition.start();
+
+// Event Listener
+
+recognition.addEventListener('result', getSpeakWord);
