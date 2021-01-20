def11fa07fc2933326efa1bcb9ec210cdfe7f9ff
JesseFang <marvin99988@gmail.com>
2021-01-20 22:28:04 +0800

    check number and add result to DOM

--- 9_speak_number_guess/script.js
+++ 9_speak_number_guess/script.js
@@ -8,6 +8,7 @@ function generateRandomNumber() {
 function getSpeakWord(e) {
     const msg = e.results[0][0].transcript;
     showMessage(msg);
+    checkNumber(msg);
 }
 
 function showMessage(msg) {
@@ -16,6 +17,31 @@ function showMessage(msg) {
     <span class="box">${msg}</span>
     `)
 }
+function checkNumber(msg) {
+    const number = +msg;
+
+    if (Number.isNaN(number)) {
+        $('#msg').append('<div>That is not a valid number</div>');
+      return;
+    }
+
+    if (number > 100 || number < 1) {
+        $('#msg').append('<div>Number must be between 1 and 100</div>') ;
+      return;
+    }
+  
+    if (number=== randomNumber) {
+      $('body').html(`
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${number}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `);
+    } else if (number > randomNumber) {
+        $('#msg').append('<div>GO LOWER</div>');
+    } else {
+        $('#msg').append('<div>GO HIGHER</div>');
+    }
+  }
 
 window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
