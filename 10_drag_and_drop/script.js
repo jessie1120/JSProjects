b430234f6b0da9b8e1ce5187d0ab8a5a7e1889a7
JesseFang <marvin99988@gmail.com>
2021-01-24 16:04:04 +0800

    add popular series data into array

---
+++ 10_drag_and_drop/script.js
@@ -0,0 +1,13 @@
+const popularSeries = [
+    'The Queen’s Gambit',
+    'Emily in Paris',
+    'Lucifer',
+    'The Umbrella Academy',
+    'Money Heist',
+    'Dark Desire',
+    'The Crown',
+    'Friends',
+    'Yo soy Betty, la fea',
+    'Ratched'
+  ];
+  
